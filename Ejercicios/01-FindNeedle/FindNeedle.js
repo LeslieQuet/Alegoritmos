cf9a7a1c8ceebb7df8b7ded562a7c35d3a26584d
LeslieQuet <quetglaslesllie@gmail.com>
2023-05-10 13:51:50 -0300

    03-MaxValue Solved

--- Ejercicios/01-FindNeedle/FindNeedle.js
+++ Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,6 +1,13 @@
 function FindNeedle (haystack, needle) {
   // Your code here:
-
+  for( let i = 0 ; i < haystack.length; i++){
+    for(let j = 0; j < needle.length + 1; j++){ 
+      if(haystack[i+j] === needle[j]){
+        if(j === needle.length - 1) return i; 
+      } else break;
+    }
+  }
+  return -1
 }
 
-module.exports = FindNeedle
+module.exports = FindNeedle;
--- Ejercicios/03-MaxValue/MaxValue.js
+++ Ejercicios/03-MaxValue/MaxValue.js
@@ -1,6 +1,48 @@
 function MaxValue (shares) {
-  // Your code here:
+//Solución refactor 2
 
+  let bestBuy = shares[0]; 
+  let bestSell = shares[1]; 
+
+  for(let i = 0; i < shares.length; i++){
+    if(shares[i] < bestBuy) bestBuy = shares[i]; 
+    if(shares[i+1] > bestSell) bestSell = shares[i+1]; // 11 > 5
+  }
+  return bestSell - bestBuy; // 11 - 2 = 9
 }
+
+//Complejidad >>> O(n) - Solución refactor
+//function MaxValue (shares) {
+//   let maxProfit = -Infinity;
+//   let minValue = shares[0];
+
+//   for(let i = 1; i < shares.length; i++){
+//     const sell = shares[i];
+//     const actualProfit =  sell - minValue;
+//     // if(actualProfit > maxProfit) maxProfit = actualProfit;
+//     maxProfit = actualProfit > maxProfit ? actualProfit : maxProfit;
+//     // if(sell < minValue) minValue = sell;
+//     minValue = sell < minValue ? sell : minValue;
+//   }
+
+//   return maxProfit;
+// }
+
+
+
+// Complejidad >>> O(n^2) - Solución fuerza bruta 
+// function MaxValue (shares) {
+//   let maxProfit = -Infinity;
+
+//   for(var i = 0; i < shares.length -1; i++){
+//     const buy = shares[i];
+//     for(var j = i + 1; j < shares.length; j++){
+//       const sell = shares[j];
+//       const actualProfit = sell - buy; 
+//       if(actualProfit > maxProfit) maxProfit = actualProfit;
+//     }
+//   }
+//   return maxProfit;
+// }
 
 module.exports = MaxValue
