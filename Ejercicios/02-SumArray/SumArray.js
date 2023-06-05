530a735ee41f63e6e9262029eca574bfb711f15c
LeslieQuet <quetglasleslie@gmail.com>
2023-06-05 10:46:21 -0300

    02-SumArray solved

--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -1,6 +1,14 @@
 function SumArray (arr, n) {
   // Your code here:
 
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === n) return true;
+    for(let j = arr.length - 1; j > 0; j--){
+      if(arr[i] + arr[j] === n) return true;
+      if(arr[i] === arr[j]) break;
+    }
+  } 
+  return false;
 }
 
 module.exports = SumArray
