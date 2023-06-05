cd89329b55b7e4d9a2505321a2ab3df07258d8bd
LeslieQuet <quetglasleslie@gmail.com>
2023-06-05 14:41:38 -0300

    06-HasBalancedBrackets Solved

--- Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
+++ Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,6 +1,26 @@
 function HasBalancedBrackets (string) {
   // Your code here:
 
-}
+  const openBrackets = ['{', '[', '('];
+  const closeBrackets = ['}', ']', ')'];
+  const openedBrackets = [];
 
+    for(let i = 0; i < string.length; i++){
+      const bracket = string[i];
+  
+      if(openBrackets.includes(bracket)) openedBrackets.push(bracket);
+  
+      if(closeBrackets.includes(bracket)){
+          const index = closeBrackets.indexOf(bracket);
+          const par = openBrackets[index];
+          const lastOpen = openedBrackets[openedBrackets.length -1];
+    
+          if (lastOpen === par)openedBrackets.pop();
+          else return false;
+        }
+      }
+      if(openedBrackets.length)return false;
+      return true;
+}
+      
 module.exports = HasBalancedBrackets
