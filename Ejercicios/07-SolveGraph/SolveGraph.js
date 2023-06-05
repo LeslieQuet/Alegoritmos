b38c314559200a641b51c2f7767bb4251dfac290
LeslieQuet <quetglasleslie@gmail.com>
2023-06-05 14:21:06 -0300

    07-SolveGraph Solved
    07-SolveGraph solved

--- Ejercicios/07-SolveGraph/SolveGraph.js
+++ Ejercicios/07-SolveGraph/SolveGraph.js
@@ -1,6 +1,28 @@
-function SolveGraph (graph, start, end) {
-  // Your code here:
+//Solución más eficiente, usa un objeto en lugar de un array (se revisa directamente la propiedad sin iterar)
+function SolveGraph (graph, start, target, visited = {}) {
 
+  if(visited[start]) return;
+  visited[start] = true;
+
+  for(const node of graph[start]){
+    if(node === target) return true;
+    if(SolveGraph(graph, node, target, visited)) return true;
+  }
+  return false;
 }
+
+
+//Solución fuerza bruta
+// function SolveGraph (graph, start, target, visited = []) {
+
+//   if(visited.includes(start)) return;
+//   visited.push(start);
+
+//   for(const node of graph[start]){
+//     if(node === target) return true;
+//     if(SolveGraph(graph, node, target, visited)) return true;
+//   }
+//   return false;
+// }
 
 module.exports = SolveGraph
